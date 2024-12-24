0f7c225c717bd1c72449c8ec35a15b677ca3727f
mtrzc0 <trzeciak.3mateusz@gmail.com>
2024-12-24 12:43:58 +0100

    Added information about planning class, priority, children and files open by the biggest process

--- process.sh
+++ process.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-function process_info () 
+function process_count ()
 {
     # separator
     COOL_LINES="--------------------------------------------------"
 
     # get number of system processes
     process_count=$(ps -e | tr -s ' ' | cut -d ' ' -f2 | grep -v PID | wc -l)
+    echo $COOL_LINES
+    echo "Number of system processes: $process_count"
+    echo $COOL_LINES
+
+    exit 0 # ERROR CODE 0 - success
+}
+
+
+function process_big () 
+{
+    # separator
+    COOL_LINES="--------------------------------------------------"
 
     # get the biggest process size and name
-    size=$(ps -eo size --sort size | tail -1)
-    name=$(ps -eo cmd --sort=size | tail -1)
+    name=$(ps -eo cmd --sort=size | tail -1 | tr -s ' ' | cut -d ' ' -f1)
+    pid=$(ps -eo pid --sort=size | tail -1 | tr -s ' ' | cut -d ' ' -f2)
+    size=$(ps -eo size --sort=size | tail -1 | tr -s ' ' | cut -d ' ' -f1)
+    class=$(ps -eco cls --sort=size | tail -1 | tr -s ' ' | cut -d ' ' -f2)
+    priority=$(ps -eo priority --sort=size | tail -1 | tr -s ' ' | cut -d ' ' -f2)
+    children=$(pgrep --parent $pid | wc -l)
+    files_open=$(lsof -p $pid | wc -l)
 
     if [[ size -le 0 ]]; then
         echo "Invald process size"
@@ -18,11 +35,32 @@ function process_info ()
     fi
 
     echo $COOL_LINES
-    echo "Number of system processes: $process_count"
-    echo $COOL_LINES
     echo "name: $name"
+    echo "pid: $pid"
     echo "size: $size KB"
+    echo "planning class: $class"
+    echo "priority: $priority"
+    echo "children: $children"
+    echo "files open: $files_open"
+    #ls -l "/proc/$pid/status"
     echo $COOL_LINES
+    #TODO
+
     exit 0 # ERROR CODE 0 - success
 }
 
+function process_main ()
+{
+    OPTIONS="Count Biggest"
+
+    echo "Which information about process to display? "
+    PS3="> "
+    select option in $OPTIONS
+    do
+        case $REPLY in
+            1) process_count    ;;
+            2) process_big      ;;
+            *) exit 0           ;;    
+        esac
+    done
+}
--- tool.sh
+++ tool.sh
@@ -5,12 +5,14 @@
 
 OPTIONS="Processes Threads Memory I/O Files"
 
+clear
+
 echo "Choose a program to run"
 PS3="> "
 select option in $OPTIONS
 do
     case $REPLY in
-        1) process_info ;;
+        1) process_main ;;
         2) threads_info ;;
         *) exit 0       ;;    
     esac
