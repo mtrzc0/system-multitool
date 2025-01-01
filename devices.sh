9f9999807ca09857d01def2ef9f49d94a0d261ea
mtrzc0 <trzeciak.3mateusz@gmail.com>
2025-01-01 18:40:49 +0100

    Added devices section with all functions

--- devices.sh
+++ devices.sh
@@ -10,71 +10,85 @@ function devices_name_size_type_disk ()
     echo $COOL_LINES
 }
 
-function memory_active_main ()
+function devices_block_uniq_major_number ()
 {
     # separator
     COOL_LINES="--------------------------------------------------"
 
     echo $COOL_LINES
-    cat /proc/meminfo | grep -E 'Active'
+    temp=$(ls -l /dev | grep ^b | tr -s ' ' | cut -d ' ' -f 5 | uniq -cu)
+    ls -l /dev | grep ^b | tr -s ' ' | cut -d ' ' -f 5,10 | uniq -c | grep "$temp" | tr -s ' '
     echo $COOL_LINES
 }
 
-function memory_inactive_main ()
+function devices_name_size_type_main ()
 {
     # separator
     COOL_LINES="--------------------------------------------------"
 
     echo $COOL_LINES
-    cat /proc/meminfo | grep -E 'Inactive'
+    sudo dmidecode --type memory | grep -E 'Size|Type'
     echo $COOL_LINES
 }
 
-function memory_page_4k_2M_main ()
+function devices_char_uniq_major_number ()
 {
     # separator
     COOL_LINES="--------------------------------------------------"
 
     echo $COOL_LINES
-    cat /proc/meminfo | grep -E 'DirectMap4k|DirectMap2M'
+    temp=$(ls -l /dev | grep ^c | tr -s ' ' | cut -d ' ' -f 5 | uniq -cu)
+    ls -l /dev | grep ^c | tr -s ' ' | cut -d ' ' -f 5,10 | uniq -c | grep "$temp" | tr -s ' '
     echo $COOL_LINES
 }
 
-function memory_used_free_disk ()
+function devices_network_controller ()
 {
     # separator
     COOL_LINES="--------------------------------------------------"
 
     echo $COOL_LINES
-    df --output='source,avail,used'
+    lspci | grep -i 'network controller'
     echo $COOL_LINES
 }
 
-function memory_used_free_partition_disk ()
+function devices_sockets_list ()
 {
     # separator
     COOL_LINES="--------------------------------------------------"
 
     echo $COOL_LINES
-    lsblk -o NAME,FSSIZE,FSUSED,FSAVAIL --noempty
+    ss -tunl
     echo $COOL_LINES
 }
 
-function memory_main ()
+function devices_ports_list ()
 {
-    OPTIONS="used_free_main active_main inactive_main page_4k_2M_main used_free_disk used_free_partition_disk"
+    # separator
+    COOL_LINES="--------------------------------------------------"
+
+    echo $COOL_LINES
+    ss -tun
+    echo $COOL_LINES
+}
+
+function devices_main ()
+{
+    OPTIONS="devices_name_size_type_disk devices_block_uniq_major_number devices_name_size_type_main devices_char_uniq_major_number devices_network_controller devices_network_controller devices_sockets_list devices_ports_list"
 
     echo "Which information about memory to display? "
     PS3="> "
     select option in $OPTIONS
     do
         case $REPLY in
-            1) memory_used_free_main             ;;
-            2) memory_active_main                ;;
-            3) memory_inactive_main              ;;
-            4) memory_page_4k_2M_main            ;;
-            5) memory_used_free_disk             ;;
+            1) devices_name_size_type_disk       ;;
+            2) devices_block_uniq_major_number   ;;
+            3) devices_name_size_type_main       ;;
+            4) devices_char_uniq_major_number    ;;
+            5) devices_network_controller        ;;
             6) memory_used_free_partition_disk   ;;
+            7) devices_sockets_list              ;;
+            8) devices_ports_list                ;;
             *) exit 0                            ;;    
         esac
     done
